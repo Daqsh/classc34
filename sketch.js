ef051647320fefd944dc39edf1724ba363f4e527
Daqsh <68496452+Daqsh@users.noreply.github.com>
2020-10-18 13:52:30 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+var hypnoticBall,database;
+var position;
+
+function setup(){
+    database = firebase.database();
+    console.log(database);
+    createCanvas(500,500);
+
+    hypnoticBall = createSprite(250,250,10,10);
+    hypnoticBall.ShapeColor = "red";
+
+    var hypnoticBallposition = database.ref('ball/position');
+    hypnoticBallposition.on("value",readposition,showError);
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+function writeposition(x,y){
+    database.ref('ball/position').set({
+        'x':position.x +x,
+        'y':position.y + y
+    })
+}
+function readposition(data){
+    position = data.val();
+    console.log(position.x);
+    hypnoticBall.x = position.x;
+    hypnoticBall.y = position.y;
+}
+function showError(){
+    console.log("Error in writing to the database");
+}
